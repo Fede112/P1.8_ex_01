29a73311180231b2e1def585416e33b72486e80d
Fede112 <fedebarone@gmail.com>
2019-01-21 17:37:28 +0100

    Added trigonometric functions to call from input argument

--- 01_tab.py
+++ 01_tab.py
@@ -1,17 +1,20 @@
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 
 if (len(sys.argv)!=2):
 	print("Error: wrong number of arguments!")
 	exit(1)
 
-# print(sys.argv[1])
+idx = int(sys.argv[1])
 
-def f(x):
-	return x*x
+def f1(x):
+	return x
+
+funcList = [f1, np.sin, np.cos, np.tan]
 
 xval = [ (i*0.1 - 0.5) for i in range(0,11) ]
-yval = [f(x) for x in xval]
+yval = [funcList[idx-1](x) for x in xval]
 
 print(xval)
 print(yval)
