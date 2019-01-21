090abee1afe6a0a654f48a33044263f90bb78b9c
Fede112 <fedebarone@gmail.com>
2019-01-21 21:55:19 +0100

    Update usage message

--- 01_tab.py
+++ 01_tab.py
@@ -6,6 +6,9 @@ if (len(sys.argv)!=2):
 	print("Error: only one integer argument expected!")
 	print("Usage: ")
 	print("1: f(x)=x**2")
+	print("2: f(x)=sin(x)")
+	print("3: f(x)=cos(x)")
+	print("4: f(x)=tan(x)")
 	exit(1)
 
 ind = int(sys.argv[1])
@@ -14,8 +17,6 @@ def f1(x):
 	return x
 
 funcList = [f1, np.sin, np.cos, np.tan]
-
-funcList = [f2]
 
 xval = [ (i*0.1 - 0.5) for i in range(0,11) ]
 yval = [ funcList[ind-1](x) for x in xval ]
