b26bbe70b04061224cb94de8758abcec93dbe0c9
Fede112 <fedebarone@gmail.com>
2019-01-21 17:22:35 +0100

    Implemented using different poly function depending on input

--- 01_tab.py
+++ 01_tab.py
@@ -5,13 +5,23 @@ if (len(sys.argv)!=2):
 	print("Error: wrong number of arguments!")
 	exit(1)
 
+ind = int(sys.argv[1])
+
 # print(sys.argv[1])
 
-def f(x):
+def f1(x):
+	return x
+def f2(x):
 	return x*x
+def f3(x):
+	return x*x*x
+
+funcList = [f1,f2,f3]
+
+
 
 xval = [ (i*0.1 - 0.5) for i in range(0,11) ]
-yval = [f(x) for x in xval]
+yval = [funcList[ind - 1](x) for x in xval]
 
 print(xval)
 print(yval)
