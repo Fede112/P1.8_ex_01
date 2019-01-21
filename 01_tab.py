2494ad0b0bc11492102cc84b446aad6cc4ae0b3b
Fede112 <fedebarone@gmail.com>
2019-01-21 17:14:03 +0100

    Created lists. Added a function to fill the second list.

--- 01_tab.py
+++ 01_tab.py
@@ -4,4 +4,13 @@ if (len(sys.argv)!=2):
 	print("Error: wrong number of arguments!")
 	exit(1)
 
-print(sys.argv[1])
+# print(sys.argv[1])
+
+def f(x):
+	return x*x
+
+xval = [ (i*0.1 - 0.5) for i in range(0,11) ]
+yval = [f(x) for x in xval]
+
+print(xval)
+print(yval)
